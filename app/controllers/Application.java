079f1b3633415a1b86967fcf15e9f50abb7a9ee8
Pelle Karlsson <speedyswedy@hotmail.com>
2012-04-09 21:01:10 +0200

    finished

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,26 +1,35 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import models.Task;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Application extends Controller {
+
+	static Form<Task> taskForm = form(Task.class);
 
 	public static Result index() {
 		return redirect(routes.Application.tasks());
 	}
 
 	public static Result tasks() {
-		return TODO;
+		return ok(views.html.index.render(Task.all(), taskForm));
 	}
 
 	public static Result newTask() {
-		return TODO;
+		Form<Task> filledForm = taskForm.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			return badRequest(views.html.index.render(Task.all(), filledForm));
+		} else {
+			Task.create(filledForm.get());
+			return redirect(routes.Application.tasks());
+		}
 	}
 
 	public static Result deleteTask(Long id) {
-		return TODO;
+		Task.delete(id);
+		return redirect(routes.Application.tasks());
 	}
 
 }
