79994e59a86dea3cc89c4cf4a0340b7d4ece111f
Pelle Karlsson <speedyswedy@hotmail.com>
2012-04-09 21:36:20 +0200

    controllerAndMethods

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,7 +7,19 @@ import views.html.index;
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok(index.render("Your new application is ready."));
+		return redirect(routes.Application.tasks());
+	}
+
+	public static Result tasks() {
+		return TODO;
+	}
+
+	public static Result newTask() {
+		return TODO;
+	}
+
+	public static Result deleteTask(Long id) {
+		return TODO;
 	}
 
 }
