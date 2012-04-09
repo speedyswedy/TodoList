01fae85b9f33986f3d5b47c2bcb1e05535ba6bed
Pelle Karlsson <speedyswedy@hotmail.com>
2012-04-09 21:38:57 +0200

    setupTask

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,19 +7,7 @@ import views.html.index;
 public class Application extends Controller {
 
 	public static Result index() {
-		return redirect(routes.Application.tasks());
-	}
-
-	public static Result tasks() {
-		return TODO;
-	}
-
-	public static Result newTask() {
-		return TODO;
-	}
-
-	public static Result deleteTask(Long id) {
-		return TODO;
+		return ok(index.render("Your new application is ready."));
 	}
 
 }
