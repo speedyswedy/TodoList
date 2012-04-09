803c8d5d83d6621ddce7c6701ec063c9be42c77a
Pelle Karlsson <speedyswedy@hotmail.com>
2012-04-09 20:33:01 +0200

    addControllerMethods

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,9 +6,21 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
+
+	public static Result index() {
+		return redirect(routes.Application.tasks());
+	}
+
+	public static Result tasks() {
+		return TODO;
+	}
+
+	public static Result newTask() {
+		return TODO;
+	}
+
+	public static Result deleteTask(Long id) {
+		return TODO;
+	}
+
 }
